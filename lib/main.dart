d54d69b560051800c7820832476ab369c08815fc
blood <hey-blood@foxmail.com>
2022-06-29 18:37:13 +0800

    Setting Up Project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Amazon Clone'),
+        ),
+        body: const Center(child: Text('Flutter Demo Home Page')),
+      ),
+    );
+  }
+}
