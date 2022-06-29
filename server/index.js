5fa1193fe3774c30e90dce585a644097f884cb7a
blood <hey-blood@foxmail.com>
2022-06-30 01:30:30 +0800

    Creating Server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log(`connected at port ${PORT}`);
+})
