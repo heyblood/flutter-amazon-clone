0d92d5394ed43b383124c42711f012a5b73465c7
blood <hey-blood@foxmail.com>
2022-06-30 02:28:40 +0800

    Sign up Route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,4 +8,13 @@ authRouter.get('/user', (req, res) => {
     });
 });
 
+authRouter.post('/api/signup', (req, res) => {
+    // get the data from client
+    const {name, email, password} = req.body;
+
+    // store that data in database
+
+    // return that data to the user
+});
+
 module.exports = authRouter;
