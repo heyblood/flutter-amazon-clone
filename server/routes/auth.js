ca87ae6bbb33185f63ebcfd5e6ade4a3a064178a
blood <hey-blood@foxmail.com>
2022-06-30 15:28:47 +0800

    Creating Sign In Route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 const authRouter = express.Router();
@@ -29,6 +31,46 @@ authRouter.post('/api/signup', async (req, res) => {
         });
         user = await user.save();
         res.json(user);
+    } catch (e) {
+        res.status(500).json({
+            error: e.message
+        });
+    }
+});
+
+
+// SIGN IN
+authRouter.post('/api/signin', async (req, res) => {
+    try {
+        const {
+            email,
+            password
+        } = req.body;
+
+        const user = await User.findOne({
+            email
+        });
+        if (!user) {
+            return res.status(400).json({
+                msg: 'A user with this email dose not exist!'
+            });
+        }
+
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                msg: 'Incorrect password.'
+            });
+        }
+
+        const token = jwt.sign({
+            id: user._id
+        }, 'passwordKey');
+
+        res.json({
+            token,
+            ...user._doc
+        });
     } catch (e) {
         res.status(500).json({
             error: e.message
